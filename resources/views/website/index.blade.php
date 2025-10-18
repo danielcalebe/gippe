8dc0acb70b6be2079503b49d813501f75b1e9068
Daniel Calebe <160525339+danielcalebe@users.noreply.github.com>
2025-10-18 09:04:05 -0300

    Update index.blade.php

--- resources/views/website/index.blade.php
+++ resources/views/website/index.blade.php
@@ -17,8 +17,8 @@
   <link href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
   <!-- Favicons -->
-  <link href="/assets/img/logo.png" rel="icon">
-  <link href="/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+  <link href="./assets/img/logo.png" rel="icon">
+  <link href="./assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 
   <!-- Google Fonts -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
@@ -563,7 +563,7 @@
 
 
 
-  <script src="{{/asset('assets/js/mainn.js')}}"></script>
+  <script src="{{asset('assets/js/mainn.js')}}"></script>
 
   <script src="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.11.3/js/lightbox.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.js"></script>
